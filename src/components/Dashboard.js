640022d9c23001e66ca2a958cfc941dda99b03ae
stella-zb <upperdaystudio@gmail.com>
2019-12-05 13:33:22 -0800

    declare the initial state value, import class delared loading component, once it works, set the loading state to false to hide it by default

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,9 +2,19 @@ import React, { Component } from "react";
 
 import classnames from "classnames";
 
+import Loading from "components/Loading";
+
 class Dashboard extends Component {
+  state = {
+    loading: false
+  }
+
   render() {
     const dashboardClasses = classnames("dashboard");
+    
+    if (this.state.loading) {
+      return <Loading />;
+    }
 
     return <main className={dashboardClasses} />;
   }
